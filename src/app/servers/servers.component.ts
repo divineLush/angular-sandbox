4e72a82fd4590224e72527b2a9259ae34f9f7bd1
divineLush <homeoflucifer@gmail.com>
2021-04-12 12:10:39 +0700

    two way binding & ngif

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -1,23 +1,29 @@
 import { Component, OnInit } from '@angular/core';
 
-
-
 @Component({
   selector: 'app-servers',
   templateUrl: './servers.component.html',
   styleUrls: ['./servers.component.sass']
 })
 export class ServersComponent implements OnInit {
-  allowNewServer: boolean = true;
-  serverCreationStatus: string = 'No server';
+  isServerCreated: boolean = false;
+  userName: string = '';
+  serverName: string = 'default';
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  // onUpdateServerName(event: Event) {
+  //   this.serverName = (<HTMLInputElement>event.target).value;
+  // }
+
+  onResetUserName() {
+    this.userName = '';
+  }
+
   onCreateServer() {
-    this.serverCreationStatus = 'Server created';
-    this.allowNewServer = false;
+    this.isServerCreated = true;
   }
 }
